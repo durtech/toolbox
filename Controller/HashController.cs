6d539d7df6abf8136e096acd0d32fdb965f5d89b
abmusawer <abmusawer.npa@gmail.com>
2021-01-21 18:39:57 +0430

    initial commit, there are a few bugs :)

---
+++ Controller/HashController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Toolbox.Core;
+
+namespace Toolbox.Controller
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HashController : ControllerBase
+    {
+        public HashController()
+        {
+
+        }
+        [HttpGet]
+       public string ASPDOTNETCOREPASS()
+        {
+            
+            string hashed_password = SecurePasswordHasherHelper.Hash("12345");
+            return hashed_password;
+        }
+
+
+    }
+}
